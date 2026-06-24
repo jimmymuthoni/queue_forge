5bb2db722886c21516d04eb03437f1b10b1c4067
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-06-24 13:21:21 +0300

    user access layer

--- store/users.go
+++ store/users.go
@@ -1,18 +1,23 @@
 package store
 
 import (
+	"context"
 	"database/sql"
+	"encoding/base64"
+	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-
 type User struct {
-	Id uuid.UUID `db:"id"`
-	Email string `db:"email"`
-	HashedPassword string `db:"HashedPassword"`
+	Id                   uuid.UUID `db:"id"`
+	Email                string    `db:"email"`
+	HashedPasswordBase64 string    `db:"HashedPassword"`
+	CreatedAt 			 time.Time `db:"created_at"`
 }
 
 type UserStore struct {
@@ -23,5 +28,22 @@ func NewUserStore(db *sql.DB) *UserStore {
 	return &UserStore{
 		db: sqlx.NewDb(db, "postgres"),
 	}
+}
+
+func (s *UserStore) CreateUser(ctx context.Context, email, password string) (*User, error){
+	const dml = `INSERT INTO users (email,hashed_password) VALUES ($1, $2) RETURNING *`
+	var user User
+
+	//hashing password provided by the user before inserting it to db
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to hash the password: %w", err)
+	}
+	hashedPasswordBase64 := base64.StdEncoding.EncodeToString(bytes)
+
+	if err := s.db.GetContext(ctx, &user, dml, email, hashedPasswordBase64); err != nil {
+		return nil, fmt.Errorf("Failed to insert the user")
+	}
+	return &user, nil
 }
 
