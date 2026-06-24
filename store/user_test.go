b7f1fc0aeb18560943345f243c29d7ef6adb8c4f
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-06-24 22:08:40 +0300

    unit tests for users

--- store/user_test.go
+++ store/user_test.go
@@ -5,15 +5,20 @@ import (
 	"os"
 	"testing"
 
-	"github.com/golang-migrate/migrate"
+	migrate "github.com/golang-migrate/migrate/v4"
+    _ "github.com/golang-migrate/migrate/v4/database/postgres"
+    _ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jimmymuthoni/queue_forge/config"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUserStore(t *testing.T){
 	os.Setenv("ENV", string(config.Env_Test))
+	
+
 	conf, err := config.New()
 	require.NoError(t, err)
+	
 
 	db, err := NewPostgresDb(conf)
 	require.NoError(t, err)
@@ -21,7 +26,7 @@ func TestUserStore(t *testing.T){
 
 	//migations to the test_db
 	m, err := migrate.New(
-		"file//migrations",
+		"file://../migrations",
 		conf.DatabaseUrl())
 	require.NoError(t, err)
 
@@ -30,6 +35,11 @@ func TestUserStore(t *testing.T){
 	}
 
 	userStore := NewUserStore(db)
-	userStore.CreateUser(context.Background(), "jim@testingmail.com", "testingpassword")
+	user, err := userStore.CreateUser(context.Background(), "jim@testingmail.com", "testingpassword")
+	require.NoError(t, err)
+
+	require.Equal(t, "jim@testingmail.com", user.Email)
+	require.NoError(t, user.ComparePassword("testingpassword"))
+
 
 }
