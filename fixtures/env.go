59a00f65be2d2aae98c40c304b275c54dc629843
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-06-25 17:53:24 +0300

    Test for migrations

--- fixtures/env.go
+++ fixtures/env.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"os"
 	"testing"
+	migrate "github.com/golang-migrate/migrate/v4"
 
 	"github.com/jimmymuthoni/queue_forge/config"
 	"github.com/jimmymuthoni/queue_forge/store"
@@ -29,4 +30,15 @@ func NewTestEnv(t *testing.T) *TestEnv {
 	}
 }
 
+//test for migrations
+func (te *TestEnv) SetUpDb(t *testing.T) {
+	m, err := migrate.New(
+		"file://../migrations",
+		te.Config.DatabaseUrl())
+	require.NoError(t, err)
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		require.NoError(t, err)
+	}
+}
 
