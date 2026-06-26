10a2c6776d2ab16c4559432aff4a8f20a44d02c8
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-06-26 11:27:57 +0300

    server

--- apiserver/server.go
+++ apiserver/server.go
@@ -1,14 +1,35 @@
 package apiserver
 
-import "github.com/jimmymuthoni/queue_forge/config"
+import (
+	"context"
+	"net/http"
+
+	"github.com/jimmymuthoni/queue_forge/config"
+)
 
 type ApiServer struct {
 	Config *config.Config
 
 }
 
-
 func New(config *config.Config) *ApiServer {
 	return &ApiServer{Config: config}
 }
 
+//this function ping to the server before starting
+func (s *ApiServer) ping(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("pong"))
+}
+
+
+//this function stsrts the server
+func (s *ApiServer) Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", s.ping)
+	server := &http.Server{
+		Addr: ":5000",
+		Handler: mux,
+	}
+	return server.ListenAndServe()
+}
