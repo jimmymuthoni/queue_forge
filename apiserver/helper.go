f099458f851b9815a5731cd65559a3473d04dacd
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-07-03 12:00:39 +0300

    encoder and decoder request helpers

--- apiserver/helper.go
+++ apiserver/helper.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 )
@@ -17,7 +18,15 @@ func (e *ErrorWithStatus) Error() string {
 	return e.err.Error()
 }
 
+func NewErrWithStatus(status int, err error) *ErrorWithStatus {
+	return &ErrorWithStatus{
+		status: status,
+		err: err,
+	}
+}
 
+
+//error response helper
 func handler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
@@ -26,7 +35,7 @@ func handler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerF
 			if e, ok := err.(*ErrorWithStatus); ok {
 				status = e.status
 				msg = http.StatusText(e.status)
-				if status == http.StatusBadRequest {
+				if status == http.StatusBadRequest || status == http.StatusConflict {
 					msg = e.err.Error()
 				}				
 
@@ -40,4 +49,31 @@ func handler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerF
 			}
 		}
 	}
+}
+
+
+//encode helper
+func encode[T any](v T, status int, w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encoding reposnse: %w", err)
+	}
+	return nil
+}
+
+
+type Varidator interface {
+	Validate() error
+}
+//decode helper
+func decode[T Varidator](r *http.Request) (T,error) {
+	var t T
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		return t, fmt.Errorf("decoding request body: %w", err)
+	}
+	if err := t.Validate(); err != nil {
+		return t, err
+	}
+	return t, nil
 }
