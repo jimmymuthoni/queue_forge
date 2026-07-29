29f9bbd8d624f90c41d4f707fc9225a13f5dfc27
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-07-29 18:52:43 +0300

    persisting refresh token into the database

--- store/refresh_token.go
+++ store/refresh_token.go
@@ -3,11 +3,23 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
+
+
+type RefreshToken struct{
+	UserId uuid.UUID	 	`db:"user_id"`
+	HashedToken	string		`db:"hashed_token"`
+	CreatedAt	time.Time	`db:"created_at"`
+	ExpiresAt	time.Time 	`db:"expires_at"`
+}
+
 
 type RefreshTokenStore struct {
 	db *sqlx.DB
@@ -19,4 +31,7 @@ func NewRefreshTokenStore(db *sql.DB) *RefreshTokenStore {
 	}
 }
 
-func (s *RefreshTokenStore) Create(ctx context.Context, token *jwt.Token) ()
+
+func (s *RefreshTokenStore) Create(ctx context.Context, token *jwt.Token) (*RefreshToken, error) {
+	const insert = `INSERT INTO refresh_tokens (user_id, hashed_token, expires_at) VALUES ($1, $2, $3)`;
+}
