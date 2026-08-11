5ceb5548edb3b56c99d3f5a811b4f7b3577a8488
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-08-11 08:40:01 +0300

    added refreshToken to the store struct

--- store/store.go
+++ store/store.go
@@ -4,10 +4,12 @@ import "database/sql"
 
 type Store struct {
 	Users *UserStore
+	RefreshTokenStore *RefreshTokenStore
 }
 
 func New(db *sql.DB) *Store {
 	return &Store{
 		Users: NewUserStore(db),
+		RefreshTokenStore: NewRefreshTokenStore(db),
 	}
 }
