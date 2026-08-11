01ae223f21e0360f3bfa10704bfa57dcb2f85ed7
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-08-11 08:15:23 +0300

    unit test for refresh tokens

---
+++ store/refresh_tokens_test.go
@@ -0,0 +1,44 @@
+package store_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jimmymuthoni/queue_forge/apiserver"
+	"github.com/jimmymuthoni/queue_forge/fixtures"
+	"github.com/jimmymuthoni/queue_forge/store"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRefreshTokenStore(t *testing.T){
+	env := fixtures.NewTestEnv(t)
+	cleanup := env.SetUpDb(t)
+	t.Cleanup(func ()  {
+		cleanup(t)
+	})
+
+	ctx := context.Background()
+	refreshTokenStore := store.NewRefreshTokenStore(env.Db)
+	userStore := store.NewUserStore(env.Db)
+	user, err := userStore.CreateUser(ctx, "test@gmail.com", "test")
+	require.NoError(t, err)
+
+	jwtManager := apiserver.NewJwtManager(env.Config)
+	tokenPair, err := jwtManager.GenerateTokenPair(user.Id)
+	require.NoError(t, err)
+
+
+	refreshTokenRecord, err := refreshTokenStore.Create(ctx,user.Id, tokenPair.RefreshToken)
+	require.NoError(t, err)
+	require.Equal(t, user.Id, refreshTokenRecord.UserId)
+	expectedExpiration, err := tokenPair.RefreshToken.Claims.GetExpirationTime()
+	require.Equal(t, expectedExpiration.Time.UnixMilli(),refreshTokenRecord.ExpiresAt.UnixMilli())
+
+	refreshTokenRecord2, err := refreshTokenStore.ByPrimaryKey(ctx, user.Id, tokenPair.RefreshToken)
+	require.Equal(t, refreshTokenRecord.UserId, refreshTokenRecord2.UserId)
+	require.Equal(t, refreshTokenRecord.HashedToken, refreshTokenRecord2.HashedToken)
+	require.Equal(t, refreshTokenRecord.CreatedAt, refreshTokenRecord2.CreatedAt)
+	require.Equal(t, refreshTokenRecord.ExpiresAt, refreshTokenRecord2.ExpiresAt)
+
+}
+
