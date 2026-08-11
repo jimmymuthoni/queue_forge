ef8fc30fe655ddf975f6234d2c77a26e51c6338c
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-08-11 07:26:33 +0300

    gerated refresh token and persisted it to the database

--- store/refresh_token.go
+++ store/refresh_token.go
@@ -3,9 +3,12 @@ package store
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/jmoiron/sqlx"
@@ -31,7 +34,19 @@ func NewRefreshTokenStore(db *sql.DB) *RefreshTokenStore {
 	}
 }
 
-
-func (s *RefreshTokenStore) Create(ctx context.Context, token *jwt.Token) (*RefreshToken, error) {
+// creating refresh token and adding it to the database
+func (s *RefreshTokenStore) Create(ctx context.Context, user_id uuid.UUID, token *jwt.Token) (*RefreshToken, error) {
 	const insert = `INSERT INTO refresh_tokens (user_id, hashed_token, expires_at) VALUES ($1, $2, $3)`;
+	hashedToken, err := bcrypt.GenerateFromPassword([]byte(token.Raw), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("bcrypt.GenerateFromPassword: %w", err)
+	}
+	base64TokenHash := base64.StdEncoding.EncodeToString(hashedToken)
+
+	var refreshToken RefreshToken
+	if err := s.db.GetContext(ctx, &refreshToken, insert, user_id, base64TokenHash); err != nil {
+		return nil, fmt.Errorf("failed to create refresh token record: %w", err)
+	}
+	return  &refreshToken, nil
+	
 }
