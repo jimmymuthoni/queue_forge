3ab035669f0e922e6a177e551e9860be1c8e0e47
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-08-11 08:26:15 +0300

    Unit tests for refresh data access layer

--- store/refresh_tokens_test.go
+++ store/refresh_tokens_test.go
@@ -40,5 +40,12 @@ func TestRefreshTokenStore(t *testing.T){
 	require.Equal(t, refreshTokenRecord.CreatedAt, refreshTokenRecord2.CreatedAt)
 	require.Equal(t, refreshTokenRecord.ExpiresAt, refreshTokenRecord2.ExpiresAt)
 
+	result, err := refreshTokenStore.DeleteUserTokens(ctx ,user.Id)
+	require.NoError(t, err)
+	rowsAffected, err := result.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), rowsAffected)
+
+
 }
 
